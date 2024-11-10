32efd8b534d8e7076094c6d7678246b469c4ce9d
Diane Corriette <dcorriette@gmail.com>
2024-11-10 19:15:01 +0000

    create main urls.py file

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,34 @@
-"""
-URL configuration for djangify_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def api_root(request):
+    return Response(
+        {
+            "blog": request.build_absolute_uri("/api/v1/blog/"),
+            "portfolio": request.build_absolute_uri("/api/v1/portfolio/"),
+        }
+    )
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path(
+        "summernote/", include("django_summernote.urls")
+    ),  # Make sure this is before other URLs
+    path("api/", include("apps.core.urls")),
+    path("api/v1", api_root, name="api-root"),
+    path("api/v1/", api_root, name="api-root"),
+    path("api/v1/blog/", include("apps.blog.urls")),
+    path("api/v1/portfolio/", include("apps.portfolio.urls")),
 ]
+
+# Add static and media URL patterns
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
